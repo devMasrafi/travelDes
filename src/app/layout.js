008f2a0344fe9b02a539f8f012964309752fbd9f
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-11-20 14:06:23 +0600

    update on loading phase

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,25 +6,27 @@ import Header from "./(upperNav)/header/page";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { useEffect, useState } from "react";
 
+import ClipLoader from "react-spinners/ClipLoader";
+
 export default function RootLayout({ children }) {
-  const [isLoading, setIsLoading] = useState(true); // State to show loading screen
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate initialization and theme fetching
+
     const savedTheme = localStorage.getItem("theme") || "light";
     document.documentElement.setAttribute("data-theme", savedTheme);
 
-    // Hide loading screen after a small delay for smooth experience
+    // Hide loading screen
     const timer = setTimeout(() => setIsLoading(false), 500);
-    return () => clearTimeout(timer); // Cleanup timeout on unmount
+    return () => clearTimeout(timer); 
   }, []);
 
   if (isLoading) {
     return (
       <html lang="en">
         <body>
-          <div className="loading-screen">
-            <p>Loading...</p>
+          <div className="flex justify-center items-center h-screen w-full bg-[var(--background-color)] " >
+          <ClipLoader color="#3498db" size={50} loading={true} />
           </div>
         </body>
       </html>
