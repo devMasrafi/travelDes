0f995d08805014b594cfe7771bd20686c650ceeb
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-11-23 12:58:30 +0600

    Hamburger nav Update

--- src/app/(upperNav)/navBar/page.jsx
+++ src/app/(upperNav)/navBar/page.jsx
@@ -11,21 +11,54 @@ const NavBar = () => {
   const [navToggle, setNavToggle] = useState(false);
 
   const onToggleHandler = () => {
-    setNavToggle(true);
+    setNavToggle(!navToggle);
   };
 
   return (
     <section className="container mx-auto mt-8">
       <div className="sm:px-1 lg:px-0 flex justify-between items-center">
-        <div className="relative w-full flex justify-between items-center">
+      <div className="relative w-full flex justify-between items-center">
+          {/* Search Bar */}
           <div>
-            <div className="sm:absolute lg:relative top-[-0.4rem] lg:top-[0.2rem]  ">
+            <div className="sm:absolute lg:relative top-[-0.4rem] lg:top-[0.2rem]">
               <SearchBar />
             </div>
           </div>
-          <div className="sm:text-3xl lg:hidden">
+
+          {/* Hamburger Icon */}
+          <div
+            onClick={onToggleHandler}
+            className="sm:text-3xl lg:hidden relative z-[10]"
+          >
             <GiHamburgerMenu />
           </div>
+
+          {/* Dropdown Menu */}
+          {navToggle && (
+            <div className={`absolute top-12 right-0 shadow-md rounded-md w-full max-w-xs p-4 z-[5] ${ theme === 'light'? 'bg-grayCol' : 'bg-white'} `}>
+              <ul className={`flex flex-col gap-y-2 text-black capitalize font-medium 
+                `}>
+                <li>
+                  <Link className={`${theme === 'light' ? 'text-white': 'text-black'}`} href="/">home</Link>
+                </li>
+                <li>
+                  <Link className={`${theme === 'light' ? 'text-white': 'text-black'}`} href="/about">about</Link>
+                </li>
+                <li>
+                  <Link className={`${theme === 'light' ? 'text-white': 'text-black'}`} href="/destination">destination</Link>
+                </li>
+                <li>
+                  <Link className={`${theme === 'light' ? 'text-white': 'text-black'}`} href="/tourPackege">tour packege</Link>
+                </li>
+                <li>
+                  <Link className={`${theme === 'light' ? 'text-white': 'text-black'}`} href="/news">news</Link>
+                </li>
+                <li>
+                  <Link className={`${theme === 'light' ? 'text-white': 'text-black'}`} href="/contact">contact</Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
 
         {/* navigation pages */}
